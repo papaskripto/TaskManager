e5bc6d63a5a8d2dbe641973393cd122c74d0a9d9
papaskripto <bygeoffreyouma@gmail.com>
2026-06-08 13:09:22 +0300

    Create DB schema

---
+++ task-manager/src/test/java/com/papaskripto/taskmanager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papaskripto.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
